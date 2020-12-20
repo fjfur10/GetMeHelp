3c7210b0f731a56999673b39a0840db9d3090156
cooperemily02 <ecooper@umass.edu>
2020-12-19 23:31:54 -0500

    making code look pretty
    beep boop

--- GetMeHelp/ViewController.swift
+++ GetMeHelp/ViewController.swift
@@ -13,97 +13,91 @@ import MapKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, CLLocationManagerDelegate, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate, DataEnteredDelegate{
     
+    
+    //Declaring outlets
     @IBOutlet weak var doneButton: UIButton!
-    
     @IBOutlet weak var optionPicker: UIPickerView!
-    
     @IBOutlet weak var tableView: UITableView!
-    
     @IBOutlet private var mapView: MKMapView!
 
+    
+    //declarations
     var locationManager = CLLocationManager()
     let request = MKDirections.Request()
-    
+    let annotation = MKPointAnnotation()
    
     
-    var pickerData = [String : String]()
-    var placescoords=[String: CLLocationCoordinate2D]()
-    let annotation = MKPointAnnotation()
+    var placescoords=[String: CLLocationCoordinate2D]() //dictionary of places matched to their coordinates
+    let pickerData = ["Banks" : "bank", "Places to Eat" : "restaurant", "Grocery Store" : "food-grocery", "Places to Stay" : "hotel-lodging", "Doctor": "doctor", "Pharmacy": "pharmacy", "Library": "library", "Religous Centers": "religion", "Malls": "shopping-mall", "Gyms": "gym", "Favorites" : "nil"] //dictionary of id's for SDK
     
+    //array initializations
     var placeArr = [Place]()
     var addressesarray = [RadarAddress]()
     var favorites = [RadarPlace]()
-    let test = ["Test"]
+    
+    //inital values
     var lat = 1.0
     var long = 1.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        pickerData = ["Banks" : "bank", "Places to Eat" : "restaurant", "Grocery Store" : "food-grocery", "Places to Stay" : "hotel-lodging", "Doctor": "doctor", "Pharmacy": "pharmacy", "Library": "library", "Religous Centers": "religion", "Malls": "shopping-mall", "Gyms": "gym", "Favorites" : "nil"]
-        // Ask for Authorisation from the User.
+        //Location Manager Stuff
         self.locationManager.requestAlwaysAuthorization()
-
-        // For use in foreground
         self.locationManager.requestWhenInUseAuthorization()
-
         if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
             locationManager.startUpdatingLocation()
         }
-        optionPicker.dataSource = self
+        
+        //delegate stuff
         optionPicker.delegate = self
-        tableView.dataSource = self
         tableView.delegate = self
+        optionPicker.dataSource = self
+        tableView.dataSource = self
+       
         tableView.register(PlacesCell.self, forCellReuseIdentifier: "cellReuseIdentifier")
     }
+    
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
-    }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        let data =  Array(pickerData.keys)
-        return data[row]
-    }
     
+    //Button to fetch places
     @IBAction func didTouchDoneButton(_ sender: UIButton) {
 
-        if(!CLLocationManager.locationServicesEnabled())
-        {
-            locationAlert()
-        }
-        
         let loc = CLLocation(latitude: lat, longitude: long)
-        print(loc.coordinate.latitude)
-        if(loc.coordinate.latitude == 1.0)
+        if(!CLLocationManager.locationServicesEnabled() || loc.coordinate.latitude == 1.0) //can a location be found?
         {
             locationAlert()
         }
+        
+        //using Radar to find nearby places
         let vals = Array(pickerData.values)
-        
         var rad = 5000
-        
-        while(findplaces(radius: rad, val: vals, myloc: loc) == false)
+        while(findplaces(radius: rad, val: vals, myloc: loc) == false) //keeps checking with a larger radius when no places are returned
         {
             rad = rad + 1000
             continue
         }
             
-        let uloc = CLLocation(latitude: lat, longitude: long)
+        //updates map view
         let regionradius:CLLocationDistance = 5000.0
-        let region = MKCoordinateRegion(center: uloc.coordinate, latitudinalMeters: regionradius, longitudinalMeters: regionradius)
+        let region = MKCoordinateRegion(center: CLLocation(latitude: lat, longitude: long).coordinate, latitudinalMeters: regionradius, longitudinalMeters: regionradius)
         mapView.setRegion(region, animated: true)
         mapView.delegate = self
         placeAnnotationHere()
     }
     
     
+    //Main function that uses Radar API/SDK to fetch nearby places based on used coordinates
     func findplaces(radius: Int, val: [Dictionary<String, String>.Values.Element], myloc: CLLocation) -> Bool
     {
-        var didit = true
+        var placesreturned = true
         Radar.searchPlaces(
           near: myloc,
           radius: Int32(radius), // meters
@@ -114,11 +108,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         ) { (status, location, places) in
             self.placeArr = [Place]()
             if(self.placeArr.count == 0 && radius<7000){
-                didit = false
-                print("\n \(Int(radius))")
-                print("going again...")
+                placesreturned = false
             }
-          // do something with places
             for place in places! {
                 print(place.name)
                 self.placescoords.updateValue(place.location.coordinate, forKey: place.name)
@@ -134,11 +125,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             }
             self.tableView.reloadData()
         }
-        return didit;
+        return placesreturned;
     }
     
     
-    
+    //Alert pop up
     func locationAlert()
     {
         let alert = UIAlertController(title: "Location Services Denied", message: "In order to use this app you must enable location services or enter location manually.", preferredStyle: .alert)
@@ -153,30 +144,35 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     @IBAction func zoomInButtonPressed(_ sender: Any) {
         mapView.setZoomByDelta(delta: 0.5, animated: true)
-        
-
     }
     
     @IBAction func zoomOutButtonPressed(_ sender: Any) {
         
         mapView.setZoomByDelta(delta: 2, animated: true)
-        
-
     }
     
+    //Toggles map to current user location
     @IBAction func homeButtonPressed(_ sender: Any) {
         
-        if(lat == 1.0)
+        if(lat == 1.0) //checks to see if the user has inputted a location or allowed app to use their location
         {
             locationAlert()
             return
         }
-        let uloc = CLLocation(latitude: lat, longitude: long)
+        let uloc = CLLocation(latitude: lat, longitude: long) //users current location
         let regionradius:CLLocationDistance = 500.0
-         let region = MKCoordinateRegion(center: uloc.coordinate, latitudinalMeters: regionradius, longitudinalMeters: regionradius)
-         mapView.setRegion(region, animated: true)
-         mapView.delegate = self
+        let region = MKCoordinateRegion(center: uloc.coordinate, latitudinalMeters: regionradius, longitudinalMeters: regionradius)
+        mapView.setRegion(region, animated: true)
+        mapView.delegate = self
         
+    }
+    
+    
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
+            renderer.strokeColor = UIColor.blue
+            return renderer
     }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
@@ -184,11 +180,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         lat = Double(locValue.latitude)
         long = Double(locValue.longitude)
     }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return placeArr.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier") as! PlacesCell
         let text = placeArr[indexPath.row].place.name
@@ -210,25 +204,18 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         
         let currentCell = tableView.cellForRow(at: indexPath)! as! PlacesCell
-
-        print(placescoords[ currentCell.textLabel!.text!]!)
-        
         let thiscoor =  placescoords[currentCell.textLabel!.text!]!
-        
         
         let regionradius:CLLocationDistance = 1000.0
         let region = MKCoordinateRegion(center: thiscoor, latitudinalMeters: regionradius, longitudinalMeters: regionradius)
          mapView.setRegion(region, animated: true)
          mapView.delegate = self
         
-        
-        
         annotation.coordinate = CLLocationCoordinate2D(latitude: thiscoor.latitude, longitude: thiscoor.longitude)
         annotation.title = currentCell.textLabel?.text!
         mapView.addAnnotation(annotation)
         
-        
-        
+        //draw navigation lines
         request.source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: lat, longitude: long), addressDictionary: nil))
         request.destination = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: thiscoor.latitude, longitude: thiscoor.longitude), addressDictionary: nil))
                request.requestsAlternateRoutes = true
@@ -236,8 +223,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         let directions = MKDirections(request: request)
 
-                directions.calculate { [unowned self] response, error in
-                    guard let unwrappedResponse = response else { return }
+        directions.calculate { [unowned self] response, error in
+        guard let unwrappedResponse = response else { return }
 
                     for route in unwrappedResponse.routes {
                         self.mapView.removeOverlays(mapView.overlays)
@@ -248,12 +235,16 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
                 }
     }
     
-    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-            let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
-            renderer.strokeColor = UIColor.blue
-            return renderer
-        }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let data =  Array(pickerData.keys)
+        return data[row]
+    }
     
+    //places annotation on map of current location
     func placeAnnotationHere(){
         annotation.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
         annotation.title = "You Are Here"
@@ -268,8 +259,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
     }
     
+    //change user location from automatic to manual
     func userDidEnterInformation(info:[String:String]){
-        print(info["street1"]!);
         Radar.geocode(address: "\(info["street1"]!) \(info["city"]!) \(info["state"]!)"){(status, addresses) in
             self.addressesarray = addresses!
             for address in self.addressesarray {
@@ -281,6 +272,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
     }
         
+    
+    //toggle heart
     func flipFavorite(cell: UITableViewCell) {
         let indexPath = tableView.indexPath(for: cell)
         let place = placeArr[indexPath!.row]
@@ -297,7 +290,5 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         print(favorites)
         tableView.reloadData()
     }
-    
-    
 }
 
